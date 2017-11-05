7bac8f4fb3bad043efc63ba1a8776bb9857f9b8a
Abdullah <aposevim@gmail.com>
2017-11-05 03:12:26 +0300

    - added APSCollectionViewCell - loaded product Image on APSDetailViewController - filled APSDetailViewController with selectedProduct

--- APShopping/APShopping/APSDetailViewController.swift
+++ APShopping/APShopping/APSDetailViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import PureLayout
+import Kingfisher
 
-class APSDetailViewController: APSBaseViewController {
+class APSDetailViewController: APSBaseViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate {
     var selectedProduct : APSProductDetail?
+    var selectedProductSizesArray = [APSProductSize]()
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var selectedProductTitleLabel: UILabel!
+    @IBOutlet weak var selectedProductPriceLabel: UILabel!
+    @IBOutlet weak var selectedProductSizeLabel: UILabel!
+    @IBOutlet weak var selectedProductSizeButton: UIButton!
+    @IBOutlet weak var addToCardButton: UIButton!
+    @IBOutlet weak var productSkuLabel: UILabel!
+    @IBOutlet weak var productDescriptionScrollView: UIScrollView!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.selectedProductSizesArray = (self.selectedProduct?.apsProductSize)!
+        self.selectedProductTitleLabel.text = selectedProduct?.name
+        let selectedProductSize =  String(describing:selectedProduct?.price ?? 0)
+        self.selectedProductPriceLabel.text = selectedProductSize + " AED"
+        self.productDescriptionLabel.text = "Description:\n" + (self.selectedProduct?.description)!
+    }
+    
+    override func viewWillLayoutSubviews(){
+        super.viewWillLayoutSubviews()
+        productDescriptionScrollView.contentSize = CGSize(width: productDescriptionLabel.frame.size.width, height: productDescriptionLabel.frame.size.height + 20)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,15 +44,34 @@ class APSDetailViewController: APSBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
-    */
+    
+    //2
+    func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return self.selectedProductSizesArray.count
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellIdentifier = "apsCollectionViewCell"
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? APSCollectionViewCell  else {
+            fatalError("The dequeued cell is not an instance of APSTableViewCell.")
+        }
+        var imageString = self.selectedProduct!.image
+        
+        if imageString != nil {
+            let urlString = "https://prod4.atgcdn.ae/small_light(p=zoom,of=undefined)/pub/media/catalog/product\(String(describing: imageString))"
+            let url = URL(string:urlString)
+            cell.collectionImageView.kf.setImage(with: url)
+        }
+        return cell
+    }
+
+    @IBAction func isBackButtonPressed(_ sender: Any) {
+    }
 
 }
